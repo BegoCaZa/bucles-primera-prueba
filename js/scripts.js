9edbf7461533480d77037fddb24ba27a446c7166
BegoCaZa <begoxcz@hotmail.com>
2025-03-24 11:12:43 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// const values=[6,3,4,6,7,8,7,8];
+
+// const printFirst0Numbers=()=>{
+//     for (let i=values.length; i>=0;i--){ //mientras el contador, sea menor que el largo del array
+//         console.log(values[i]);
+//         }
+// }
+// printFirst0Numbers();
+
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+    
+// // }
+
+//1️⃣ Camila está revisando una lista de nombres, (por ejemplo: ['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía']). Debe imprimir cada nombre en mayúsculas, uno por línea.
+
+const printListNames=()=>{
+    const names=['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía'];
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+        
+    }
+
+}
+    
+printListNames();
